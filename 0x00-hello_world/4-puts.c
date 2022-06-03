37a0a1376f42c8468c88184d7bc8e62a4cd3d963
Olatunde <abifarintunde@gmail.com>
2022-06-03 03:02:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)/*This is the body of the program*/
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }	
