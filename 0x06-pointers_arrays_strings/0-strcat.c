d3ed4df4e5b07519535aeae8418dd5143f79587c
Olatunde <abifarintunde@gmail.com>
2022-06-16 13:27:40 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,6 @@
  *
  *Return: Always 0.
  */
-int main(void)
 
 char *_strcat(char *dest, char *src)
 
@@ -14,7 +13,7 @@ char *_strcat(char *dest, char *src)
 int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
-for (index = 0; src[index]; index++)
+for (index = 0; src[index] && index < n; index++)
 dest[dest_len++] = src[index];
 return (dest);
 }
