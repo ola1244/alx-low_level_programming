f196c76facb3a4e881bf97fd49f377cea08cda46
Olatunde <abifarintunde@gmail.com>
2022-06-16 13:59:31 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,6 @@
  *@s1: A pointer to the first string to be compared.
  *
  *@s2: A pointer to the second string to be compared.
- *
- *
  *Return: If str1 < str2, the negative difference of the first unmatched characters.
  *
  *Return: If str1 < str2, the negative difference
@@ -21,17 +19,11 @@
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-
 while (*s1 && *s2 && *s1 == *s2)
-
 {
-
 s1++;
-
 s2++;
 }
 return (*s1 - *s2);
-
 }
