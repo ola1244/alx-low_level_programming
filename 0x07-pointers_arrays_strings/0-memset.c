9514f7e5ebb394a3a33b874e72268b898e53c123
olabisco <95924168+olabisco@users.noreply.github.com>
2022-06-21 16:04:37 +0100

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -18,7 +18,7 @@
  *Return: A pointer to the filled memory area @s.
  *
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset (char *s, char b, unsigned int n)
 {
 unsigned int index;
 char *memory = s, value = b;
