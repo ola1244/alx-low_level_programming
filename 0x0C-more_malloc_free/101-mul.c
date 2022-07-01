19b804fdd0a200facfd6ae9fbeafba8c6c7382aa
Olatunde <abifarintunde@gmail.com>
2022-07-01 08:19:18 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,20 +9,20 @@
  *Return: int
  *
  */
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
-if(argc != 3)
+if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-for(i = 1; i < argc; i++)
+for ( i = 1; i < argc; i++)
 {
-for(j = 0; argv[i][j] != '\0'; j++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-if(argv[i][j] > 57 || argv[i][j] < 48)
+if (argv[i][j] > 57 || argv[i][j] < 48)
 {
 printf("Error\n");
 exit(98);
