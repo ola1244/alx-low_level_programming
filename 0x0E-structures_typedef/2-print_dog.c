04f816cf4b44537ceb5166d604636bac30a08037
Olatunde <abifarintunde@gmail.com>
2022-07-05 09:14:29 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,9 +14,7 @@ if (d == NULL) /* validate if d initiated correctly */
 {
 return ();
 }
-(d->name == NULL) ? printf("Name: (nil)\n")
-: printf("Name: %s\n", d->name);
+(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
-(d->owner == NULL) ? printf("Owner: (nil)\n")
-: printf("Owner: %s\n", d->owner);
+(d->owner == NULL) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
